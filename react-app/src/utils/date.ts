5f12144b9800de13c3b36c06fc0781fc0bc3c1f1
Matej Voboril <tobiah@pm.me>
2023-12-05 22:30:57 -0600

    flex date calc for utc offset

--- react-app/src/utils/date.ts
+++ react-app/src/utils/date.ts
@@ -1,4 +1,10 @@
 export const startOfDay = (instant: string | number | Date): Date => {
-    const d = new Date(instant);
-    return new Date(Date.UTC(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate()))
+  const d = new Date(instant);
+  if (d.getTimezoneOffset() < 0) {
+    return new Date(Date.UTC(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate()));
+  }
+  if (d.getTimezoneOffset() > 0) {
+    return new Date(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate());
+  }
+  return d;
 };
