2f44d364e25ef00376d88acaa49c23375173fa8b
Matej Voboril <tobiah@pm.me>
2024-03-13 23:06:26 -0500

    build: avoid test trying to compile with app

--- react-app/src/__tests__/RomcalApp.test.tsx
+++ react-app/src/__tests__/RomcalApp.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { render, screen } from '@testing-library/react';
-import RomcalApp from './RomcalApp';
+import RomcalApp from '../RomcalApp';
 
 import '@testing-library/jest-dom'
 
