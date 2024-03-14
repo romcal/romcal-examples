e86fe26bbda11344adb39e6c66b93748d9c96079
Matej Voboril <tobiah@pm.me>
2024-03-13 22:28:53 -0500

    test: fix the dom test

--- react-app/src/RomcalApp.test.tsx
+++ react-app/src/RomcalApp.test.tsx
@@ -1,6 +1,9 @@
-import { render, screen } from '@testing-library/react';
 import React from 'react';
+
+import { render, screen } from '@testing-library/react';
 import RomcalApp from './RomcalApp';
+
+import '@testing-library/jest-dom'
 
 test('renders romcal headline', () => {
   render(<RomcalApp />);
